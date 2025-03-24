270a6edfbd53407c6f2b534ac1a1b1e52e9c0f0d
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-03-24 17:12:02 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
 function myButtonClicked() {
-  alert("Hello, World!")
+  //input
+  const ONTARIO_INCOME_TAX = 0.18
+  const hoursWorked = parseFloat(document.getElementById("hours-worked").value)
+  const hourlyWage = parseFloat(document.getElementById("hourly-wage").value)
+  //process
+  let incomeTaxed = hoursWorked*hourlyWage*ONTARIO_INCOME_TAX
+  let takeHomeSalary = hoursWorked*hourlyWage-incomeTaxed
+  // output
+  document.getElementById("answer1"). innerHTML = 
+    "Your pay will be: $" + takeHomeSalary.toFixed(2)
+  document.getElementById("answer2").innerHTML = 
+    "The government will take: $" + incomeTaxed.toFixed(2)
 }
